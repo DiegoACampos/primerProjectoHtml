19f04b526cc53929b0055d4faaa3ec364218002c
DiegoACampos <diegoacampos.dc@gmail.com>
2022-03-17 19:46:38 -0300

    sube seguro

--- dino-chrome/proectGame1.js
+++ dino-chrome/proectGame1.js
@@ -1,4 +1,7 @@
 const player = document.getElementById("player");
+const buttonPlayStop = document.getElementById("buttonPlayStop");
+let score = 0;
+let scoreInterval;
 
 document.addEventListener("click", function() {
     player.classList.add("playerJump")
@@ -7,3 +10,30 @@ document.addEventListener("click", function() {
 document.addEventListener("animationend", () => {
     player.classList.remove("playerJump")
 })
+
+function pauseGame() {
+    cactus1.style.animationPlayState = "paused";
+    player.style.animationPlayState = "paused";
+    fondo.style.animationPlayState = "paused";
+    clearInterval(scoreInterval);
+};
+
+function resumeGame() {
+    cactus1.style.animationPlayState = "running";
+    player.style.animationPlayState = "running";
+    fondo.style.animationPlayState = "running";
+};
+
+document.addEventListener('click', () => {
+    if (buttonPlayStop.classList.contains("play")) {
+        pauseGame();
+    } else {
+        resumeGame();
+    }
+    buttonPlayStop.classList.toggle("play");
+});
+
+scoreInterval = setInterval(() => {
+    score++;
+    document.getElementById("score").innerText = score;
+}, 100);
